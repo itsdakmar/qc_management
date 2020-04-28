c446de368485eddadb9f95840457464aeae8ac50
itsdakmar <b031810044@student.utem.edu.my>
2020-04-28 22:45:36 +0800

    update case

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Defect;
+
 class HomeController extends Controller
 {
+    public $total, $created, $inprogress, $closed;
+
     /**
      * Create a new controller instance.
      *
@@ -12,6 +16,10 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->total = Defect::count();
+        $this->created = Defect::where('status', 1)->get()->count();
+        $this->inprogress = Defect::where('status', 2)->get()->count();
+        $this->closed = Defect::where('status', 3)->get()->count();
     }
 
     /**
@@ -21,6 +29,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $totalCount = $this->total;
+        $createdCount = $this->created;
+        $inProgressCount = $this->inprogress;
+        $closedCount = $this->closed;
+
+        return view('dashboard', compact('totalCount','createdCount','inProgressCount','closedCount'));
     }
 }
