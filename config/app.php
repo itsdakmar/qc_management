9acd3f49d71482615a6605013e2b758c8850a805
itsdakmar <b031810044@student.utem.edu.my>
2020-04-28 23:03:17 +0800

    update case

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         Spatie\Permission\PermissionServiceProvider::class,
         Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
+
 
     ],
 
@@ -230,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+
     ],
 
 ];
